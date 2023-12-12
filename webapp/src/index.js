92297bba1306f4f0b88f79eb86260b9672abb234
Isuru Gunarathne <isuru623@gmail.com>
2023-12-12 14:47:43 +0530

    sample asgardeo

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GramaHome from './pages/GramaHome';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GramaHome from "./pages/GramaHome";
+import { AuthPage } from "./Auth/AuthPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <GramaHome /> */}
+    <AuthPage />
   </React.StrictMode>
 );
 
