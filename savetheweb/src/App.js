e60e9e7eb5c2cc4b01731d81117dd14ef1964038
Isuru Gunarathne <isuru623@gmail.com>
2023-12-18 15:08:29 +0530

    add complete app

--- savetheweb/src/App.js
+++ savetheweb/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import Navtop from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import CheckStatus from "./pages/CheckStatus";
+import Footer from "./components/Footer";
+import GramaCertificate from "./pages/GramaCertificate";
+import HelpForm from "./components/HelpForm";
+import GramaNilHome from "./pages/GramaNilHome";
+import MasterHome from "./pages/MasterHome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      className="App"
+      sx={{ height: "100vh", display: "flex", flexDirection: "column" }}
+    >
+      <BrowserRouter>
+        <Navtop />
+
+        <Box
+          className="pages"
+          sx={{ display: "flex", flexFlow: "column", height: "100%" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {" "}
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+
+            <Route path="/home" element={<MasterHome />} />
+            {/* change redirect upon signin to this page */}
+
+            <Route path="/gramaCertificate" element={<GramaCertificate />} />
+
+            <Route path="/checkStatus" element={<CheckStatus />} />
+
+            <Route path="/gramaNilHome" element={<GramaNilHome />} />
+
+            <Route path="/help" element={<HelpForm />} />
+
+          </Routes>
+        </Box>
+        <Footer />
+      </BrowserRouter>
+    </Box>
   );
 }
 
